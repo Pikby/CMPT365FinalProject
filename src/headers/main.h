4ae43a3a42a324d5d0c8c4e89519ea5d974fb690
pikby <jacobajensen@hotmail.ca>
2017-11-23 15:39:56 -0800

    Finished the basic transtion

--- src/headers/main.h
+++ src/headers/main.h
@@ -1,2 +1,42 @@
 using namespace cv;
-void playVideo(VideoCapture video);
+
+//The frame object
+class FrameObject
+{
+  private:
+    //The name of the new window or video
+    std::string videoName;
+
+    double fps=5;
+    //The video type for new matrices
+    int vidType;
+    //Path to the first video
+    std::string video1;
+    //Path to the second video
+    std::string video2;
+    //Resolution
+    double width = 1280;
+    double height = 720;
+    //The captures of the two videos
+    VideoCapture vid1;
+    VideoCapture vid2;
+  public:
+    FrameObject(std::string vidname, std::string pathtovid1, std::string pathtovid2)
+    {
+      videoName = vidname;
+      video1 = pathtovid1;
+      video2 = pathtovid2;
+      vid1 = VideoCapture(pathtovid1);
+      vid2 = VideoCapture(pathtovid2);
+      vidType = getVid1Frame(1).type();
+    }
+    void setResolution(int nWidth, int nHeight)
+    {
+      width = nWidth;
+      height = nHeight;
+    }
+    Mat getVid1Frame(int frame);
+    Mat getVid2Frame(int frame);
+    Mat getWipeFrame(int vid1Frame,int vid2Frame, int dist, bool startLeft);
+    void playFrame(Mat frame);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,106 @@
 
 using namespace cv;
 
-int main()
+int main(int argc, char** argv)
 {
-  VideoCapture cap("test.mp4");
-  playVideo(cap);
+  std::string video1f = "";
+  std::string video2f = "";
+
+  //Check for two input arguments and gets there file path
+  if(argc > 1)
+  {
+    video1f = argv[1];
+  }
+
+  if(argc > 2)
+  {
+    video2f = argv[2];
+  }
+  //Create the frame class in order to calculate the wipe;
+  FrameObject mainFrame("Project", video1f, video2f);
+
+
+  int curFrame = 0;
+  int dist = 0;
+  //Goes through frame by frame and makes the transition
+  while(dist< 1280)
+  {
+      std::cout << curFrame << "\n";
+      mainFrame.playFrame(mainFrame.getWipeFrame(curFrame,curFrame,dist,true));
+      std::cout << "Played frame\n";
+      curFrame++;
+      dist++;
+
+  }
+
 }
 
-void playVideo(VideoCapture video)
+//Returns the current framewipe with given parameter
+Mat FrameObject::getWipeFrame(int vid1Frame,int vid2Frame, int dist, bool startLeft)
 {
-  double fps = video.get(CV_CAP_PROP_FPS);
-  Mat frame;
-  namedWindow("Video",1);
-  while(video.isOpened())
+
+  Mat vid1mat;
+  Mat vid2mat;
+  //Extract both the frames
+  vid1mat = getVid1Frame(vid1Frame);
+  vid2mat = getVid2Frame(vid2Frame);
+
+  if(dist >= width) dist = width-1;
+  if(dist <0) dist = 0;
+
+
+  Mat finalFrame(height,width,vidType);
+  //Iterates through and calculates the new frame
+  if(startLeft)
   {
-    video >> frame;
-    imshow("Video", frame);
-    waitKey(fps);
+    for(int i = 0; i<width; i++)
+    {
+      for(int j = 0; j<height; j++)
+      {
+        if(i<dist)
+        {
+          finalFrame.at<Vec3b>(j,i) = vid2mat.at<Vec3b>(j,i);
+        }
+        else finalFrame.at<Vec3b>(j,i) = vid1mat.at<Vec3b>(j,i);
+      }
+    }
   }
+  return finalFrame;
+}
+
+//Returns the frame at a given time and makes sure its in the right resolution
+Mat FrameObject::getVid1Frame(int frame)
+{
+    if(frame>=vid1.get(CAP_PROP_FRAME_COUNT))
+    {
+      return Mat(height,width,0);
+    }
+    vid1.set(CAP_PROP_POS_FRAMES, frame);
+    int curWidth = vid1.get(CAP_PROP_FRAME_WIDTH);
+    int curHeight = vid1.get(CAP_PROP_FRAME_HEIGHT);
+    Mat tempMat,finalMat;
+    vid1 >> tempMat;
+    resize(tempMat,finalMat,finalMat.size(),(double)curWidth/width, (double)curHeight/height);
+    return finalMat;
+}
+
+Mat FrameObject::getVid2Frame(int frame)
+{
+    if(frame>=vid2.get(CAP_PROP_FRAME_COUNT))
+    {
+      return Mat(height,width,0);
+    }
+    vid2.set(CAP_PROP_POS_FRAMES, frame);
+    int curWidth = vid2.get(CAP_PROP_FRAME_WIDTH);
+    int curHeight = vid2.get(CAP_PROP_FRAME_HEIGHT);
+    Mat tempMat,finalMat;
+    vid2 >> tempMat;
+    resize(tempMat,finalMat,finalMat.size(),(double)curWidth/width, (double)curHeight/height);
+    return finalMat;
+}
+//Plays the current frame
+void FrameObject::playFrame(Mat frame)
+{
+  imshow(videoName, frame);
+  waitKey(1);
 }
